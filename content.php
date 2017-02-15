7e0369bd7772af6c9bf3f820e61142b2ba92c016
Joe Mester <joe@joemester.com>
2017-02-15 17:47:55 -0500

    update for single pages

--- content.php
+++ content.php
@@ -4,47 +4,53 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('col span_1_of_2' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<p class="full"><span class="left">Written by: <?php the_author_link(); ?></span><span class="right"> <?php _s_posted_on(); ?></span></p>
 
 		
 	</header><!-- .entry-header -->
+	<!-- <div class="featuredimage">
+		<?php 
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail('');
+			}
+			else {
+				echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/img/post-icon.jpg" />';
+			}  ?>
+		</div> -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="col span_1_of_2">
+		<div class="entry-meta">
+			<p>Written by: <?php the_author_link(); ?></p>
+			<?php _s_posted_on(); ?>
+			<?php _s_entry_footer(); ?>
+		
+		</div><!-- .entry-meta -->
+		
+		</div>
+			
+		<div class="col span_1_of_2">
+			<?php echo do_shortcode('[mashshare shares="false" buttons="true" align="right" ]'); ?> 
+			
+		</div>
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
 
 <?php
-if ( has_post_thumbnail() ) {
-	the_post_thumbnail('custpost');
-}
-else {
-	echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/assets/img/post-icon.jpg" />';
-} ?>
-
-	<div class="entry-content">
-		<?php
-		echo '<a href=', get_permalink(),'>';
-			/* translators: %s: Name of current post */
-			the_excerpt( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		echo '</a>';
-		?>
-
-		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
-		<?php if ( 'post' == get_post_type() ) : ?>
-		
-			<div class="entry-meta">
-				<?php _s_posted_on(); ?>
-			</div><!-- .entry-meta -->
-			<?php endif; ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
